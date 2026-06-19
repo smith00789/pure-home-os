5d74451ff549eb7115f5941e12685cbbc63f8c96
smith00789 <mr.sahib@mail.ru>
2026-06-19 21:17:54 +0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "ТВОЙ_SUPABASE_URL",
-  "ТВОЙ_SUPABASE_ANON_KEY"
+  npedzpwwsgcokugdvkxz
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZWR6cHd3c2djb2t1Z2R2a3h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NzY3MjYsImV4cCI6MjA5NTA1MjcyNn0.zZ87CKQ1O7Ca14pRvdiUXGRHbu-ngUyHSUgx8tHSlnY
 );
